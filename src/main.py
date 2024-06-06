91a186e9595ab79731a3c750270d81f3c8d41693
Martin Oywa <martinoywa2@gmail.com>
2024-06-06 19:08:36 +0300

    Update webapp details

--- src/main.py
+++ src/main.py
@@ -7,16 +7,52 @@ from loader import model_loader
 from preprocessor import preprocess
 
 st.title("Lung Cancer Histopathological Images Classifier")
+if st.checkbox("Data and Model Details"):
+    st.subheader("Dataset Details")
+    st.markdown("""
+        The dataset contains 25,000 histopathological images with 5 classes. All images are 768 x 768 pixels in size and are in jpeg file format.
+        The images were generated from an original sample of HIPAA compliant and validated sources, consisting of 750 total images of lung tissue (250 benign lung tissue, 250 lung adenocarcinomas, and 250 lung squamous cell carcinomas) and 500 total images of colon tissue (250 benign colon tissue and 250 colon adenocarcinomas) and augmented to 25,000 using the Augmentor package.
+        There are five classes in the dataset, each with 5,000 images, being:
+
+        - Lung benign tissue
+        - Lung adenocarcinoma
+        - Lung squamous cell carcinoma
+        - Colon adenocarcinoma
+        - Colon benign tissue
+        """)
+
+    st.markdown("---")
+
+    st.subheader("Model Details")
+    st.image("src/images/training_metrics_overtime.png", caption="Training Metrics Overtime")
+    st.markdown("""
+        ```
+        Test Loss: 0.018140
+        ```
+        """)
+    st.markdown("""
+           ```
+           Test Accuracy of Lung Adenocarcinoma: 99% (716/717)
+           Test Accuracy of Lung Benign Tissue: 100% (763/763)
+           Test Accuracy of Lung Squamous Cell Carcinoma: 97% (753/770)
+           ```
+           """)
+    st.markdown("""
+        ```
+        Test Accuracy (Overall): 99% (2232/2250)
+        ```
+        """)
+st.markdown("---")
 
 # classes
-classes = ["lung_adenocarcinoma", "lung_benign_tissue", "lung_squamous_cell_carcinoma"]
+classes = ["Lung Adenocarcinoma", "Lung Benign Tissue", "Lung Squamous Cell Carcinomaa"]
 
 # checkpoint loader
 model_path = Path("src/checkpoint/model_v4_0199.pt")
 model = model_loader(model_path)
 
 # file uploader
-image_file = st.file_uploader("Choose a file")
+image_file = st.file_uploader("Upload Lung Image", type=["png", "jpg", "jpeg"])
 
 # inference
 if image_file is not None:
@@ -32,10 +68,8 @@ if image_file is not None:
 
     # file viewer
     st.image(bytes_data, caption="Uploaded image")
-    st.write(f"Predicted Class: {classes[pred.item()]}")
-
-st.subheader("Data Details")
-st.text("Lorem Ipsum Dolor")
-
-st.subheader("Model Details")
-st.text("Lorem Ipsum Dolor")
+    st.markdown(f"""
+    ```
+    Predicted Class: {classes[pred.item()]}
+    ```
+    """)
